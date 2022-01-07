e6f2e68b2739caf580d900ab6152d5733d39cc03
HolyHigh <845803085@qq.com>
2022-01-07 23:58:04 +0800

    feat: Java日志框架学习，项目初始化

---
+++ src/main/java/com/holyhigh/loggingstudy/LoggingStudyApplication.java
@@ -0,0 +1,13 @@
+package com.holyhigh.loggingstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/holyhigh/loggingstudy/LoggingStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holyhigh.loggingstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
